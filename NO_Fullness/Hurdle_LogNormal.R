de2a155bb7bac7e32d549cd9298a84b2c746e9f2
Dodrill <mdodrill@usgs.gov>
2019-06-03 14:01:38 -0700

    Moved plotting code out

--- NO_Fullness/Hurdle_LogNormal.R
+++ NO_Fullness/Hurdle_LogNormal.R
@@ -3,22 +3,22 @@
 #                 Gut Fullness - Log-Normal Hurdle Models 
 #  
 #  Notes:
-#                     
+#  *
 #
 #  To Do:
-#  * Find out where there are NAs in the data
+#  * 
 #
 ###############################################################################
 library(rstan)
 library(brms)
 # library(ggplot2)
 library(dplyr)
-library(foodbase)
+# library(foodbase)
 # library(ggthemes)
 library(tidybayes)
 library(modelr)
-library(suncalc)
-library(arm)
+# library(suncalc)
+# library(arm)
 
 rstan_options(auto_write = TRUE)
 options (mc.cores=parallel::detectCores()) # Run on multiple cores
@@ -61,210 +61,242 @@ fit.1 = brm(bf(est.mass.g ~ ts.mean.2 + drift.mass.2 + log(Weight) + (1|no.trip.
 #-----------------------------------------------------------------------------#
 # fit a model set...
 
-fit.a.1 = brm(bf(est.mass.g ~ 1 + log(Weight) + (1|no.trip.site),
+fit.a.1 = brm(bf(est.mass.g ~ 1 + (1|no.trip.site),
                hu ~ 1 + (1|no.trip.site)),
             data = dat.in,
             family = "hurdle_lognormal",
             chains = 3, iter = 1000)
+loo.a.1 = loo(fit.a.1)
 
-fit.a.2 = brm(bf(est.mass.g ~ 1 + log(Weight) + (1|no.trip.site),
+fit.a.2 = brm(bf(est.mass.g ~ 1 + (1|no.trip.site),
                hu ~ ts.mean.2 + (1|no.trip.site)),
             data = dat.in,
             family = "hurdle_lognormal",
             chains = 3, iter = 1000)
+loo.a.2 = loo(fit.a.2)
 
-fit.a.3 = brm(bf(est.mass.g ~ 1 + log(Weight) + (1|no.trip.site),
+fit.a.3 = brm(bf(est.mass.g ~ 1 + (1|no.trip.site),
                hu ~ density.2 + (1|no.trip.site)),
             data = dat.in,
             family = "hurdle_lognormal",
             chains = 3, iter = 1000)
+loo.a.3 = loo(fit.a.3)
 
-fit.a.4 = brm(bf(est.mass.g ~ 1 + log(Weight) + (1|no.trip.site),
+fit.a.4 = brm(bf(est.mass.g ~ 1 + (1|no.trip.site),
                hu ~ mean.temp.2 + (1|no.trip.site)),
             data = dat.in,
             family = "hurdle_lognormal",
             chains = 3, iter = 1000)
+loo.a.4 = loo(fit.a.4)
 
-fit.a.5 = brm(bf(est.mass.g ~ 1 + log(Weight) + (1|no.trip.site),
+fit.a.5 = brm(bf(est.mass.g ~ 1 + (1|no.trip.site),
                hu ~ tss.2 + (1|no.trip.site)),
             data = dat.in,
             family = "hurdle_lognormal",
             chains = 3, iter = 1000)
+loo.a.5 = loo(fit.a.5)
 
-fit.a.6 = brm(bf(est.mass.g ~ 1 + log(Weight) + (1|no.trip.site),
+fit.a.6 = brm(bf(est.mass.g ~ 1 + (1|no.trip.site),
                hu ~ drift.mass.2 + (1|no.trip.site)),
             data = dat.in,
             family = "hurdle_lognormal",
             chains = 3, iter = 1000)
+loo.a.6 = loo(fit.a.6)
 
 
 
 # 2 variable models for hu
-fit.a.7 = brm(bf(est.mass.g ~ 1 + log(Weight) + (1|no.trip.site),
+fit.a.7 = brm(bf(est.mass.g ~ 1 + (1|no.trip.site),
                hu ~ ts.mean.2 + density.2 + (1|no.trip.site)),
             data = dat.in,
             family = "hurdle_lognormal",
             chains = 3, iter = 1000)
+loo.a.7 = loo(fit.a.7)
 
-fit.a.8 = brm(bf(est.mass.g ~ 1 + log(Weight) + (1|no.trip.site),
+fit.a.8 = brm(bf(est.mass.g ~ 1 + (1|no.trip.site),
                hu ~ ts.mean.2 + mean.temp.2 + (1|no.trip.site)),
             data = dat.in,
             family = "hurdle_lognormal",
             chains = 3, iter = 1000)
+loo.a.8 = loo(fit.a.8)
 
-fit.a.9 = brm(bf(est.mass.g ~ 1 + log(Weight) + (1|no.trip.site),
+fit.a.9 = brm(bf(est.mass.g ~ 1 + (1|no.trip.site),
                hu ~ ts.mean.2 + tss.2 + (1|no.trip.site)),
             data = dat.in,
             family = "hurdle_lognormal",
             chains = 3, iter = 1000)
+loo.a.9 = loo(fit.a.9)
 
-fit.a.10 = brm(bf(est.mass.g ~ 1 + log(Weight) + (1|no.trip.site),
+fit.a.10 = brm(bf(est.mass.g ~ 1 + (1|no.trip.site),
                hu ~ ts.mean.2 + drift.mass.2 + (1|no.trip.site)),
             data = dat.in,
             family = "hurdle_lognormal",
             chains = 3, iter = 1000)
+loo.a.10 = loo(fit.a.10)
 
-fit.a.11 = brm(bf(est.mass.g ~ 1 + log(Weight) + (1|no.trip.site),
+fit.a.11 = brm(bf(est.mass.g ~ 1 + (1|no.trip.site),
                   hu ~ density.2 + mean.temp.2 + (1|no.trip.site)),
             data = dat.in,
             family = "hurdle_lognormal",
             chains = 3, iter = 1000)
+loo.a.11 = loo(fit.a.11)
 
-fit.a.12 = brm(bf(est.mass.g ~ 1 + log(Weight) + (1|no.trip.site),
+fit.a.12 = brm(bf(est.mass.g ~ 1 + (1|no.trip.site),
                   hu ~ density.2 + tss.2 + (1|no.trip.site)),
             data = dat.in,
             family = "hurdle_lognormal",
             chains = 3, iter = 1000)
+loo.a.12 = loo(fit.a.12)
 
-fit.a.13 = brm(bf(est.mass.g ~ 1 + log(Weight) + (1|no.trip.site),
+fit.a.13 = brm(bf(est.mass.g ~ 1 + (1|no.trip.site),
                   hu ~ density.2 + drift.mass.2 + (1|no.trip.site)),
             data = dat.in,
             family = "hurdle_lognormal",
             chains = 3, iter = 1000)
+loo.a.13 = loo(fit.a.13)
 
-fit.a.14 = brm(bf(est.mass.g ~ 1 + log(Weight) + (1|no.trip.site),
+fit.a.14 = brm(bf(est.mass.g ~ 1 + (1|no.trip.site),
                   hu ~ mean.temp.2 + tss.2  + (1|no.trip.site)),
             data = dat.in,
             family = "hurdle_lognormal",
             chains = 3, iter = 1000)
+loo.a.14 = loo(fit.a.14)
 
-fit.a.15 = brm(bf(est.mass.g ~ 1 + log(Weight) + (1|no.trip.site),
+fit.a.15 = brm(bf(est.mass.g ~ 1 + (1|no.trip.site),
                   hu ~ mean.temp.2 + drift.mass.2 + (1|no.trip.site)),
             data = dat.in,
             family = "hurdle_lognormal",
             chains = 3, iter = 1000)
+loo.a.15 = loo(fit.a.15)
 
-fit.a.16 = brm(bf(est.mass.g ~ 1 + log(Weight) + (1|no.trip.site),
+fit.a.16 = brm(bf(est.mass.g ~ 1 + (1|no.trip.site),
                   hu ~ tss.2 + drift.mass.2 + (1|no.trip.site)),
             data = dat.in,
             family = "hurdle_lognormal",
             chains = 3, iter = 1000)
+loo.a.16 = loo(fit.a.16)
 
 
 
 # 3 variable models 
-fit.a.17 = brm(bf(est.mass.g ~ 1 + log(Weight) + (1|no.trip.site),
+fit.a.17 = brm(bf(est.mass.g ~ 1 + (1|no.trip.site),
                   hu ~ ts.mean.2 + density.2 + mean.temp.2 + (1|no.trip.site)), 
             data = dat.in,
             family = "hurdle_lognormal",
             chains = 3, iter = 1000)
+loo.a.17 = loo(fit.a.17)
 
-fit.a.18 = brm(bf(est.mass.g ~ 1 + log(Weight) + (1|no.trip.site),
+fit.a.18 = brm(bf(est.mass.g ~ 1 + (1|no.trip.site),
                   hu ~ ts.mean.2 + density.2 + tss.2 + (1|no.trip.site)), 
             data = dat.in,
             family = "hurdle_lognormal",
             chains = 3, iter = 1000)
+loo.a.18 = loo(fit.a.18)
 
-fit.a.19 = brm(bf(est.mass.g ~ 1 + log(Weight) + (1|no.trip.site),
+fit.a.19 = brm(bf(est.mass.g ~ 1 + (1|no.trip.site),
                   hu ~ ts.mean.2 + density.2 + drift.mass.2 + (1|no.trip.site)), 
             data = dat.in,
             family = "hurdle_lognormal",
             chains = 3, iter = 1000)
+loo.a.19 = loo(fit.a.19)
 
-fit.a.20 = brm(bf(est.mass.g ~ 1 + log(Weight) + (1|no.trip.site),
+fit.a.20 = brm(bf(est.mass.g ~ 1 + (1|no.trip.site),
                   hu ~ ts.mean.2 + mean.temp.2 + tss.2 + (1|no.trip.site)), 
             data = dat.in,
             family = "hurdle_lognormal",
             chains = 3, iter = 1000)
+loo.a.20 = loo(fit.a.20)
 
-fit.a.21 = brm(bf(est.mass.g ~ 1 + log(Weight) + (1|no.trip.site),
+fit.a.21 = brm(bf(est.mass.g ~ 1 + (1|no.trip.site),
                   hu ~ ts.mean.2 + mean.temp.2  + drift.mass.2 + (1|no.trip.site)), 
             data = dat.in,
             family = "hurdle_lognormal",
             chains = 3, iter = 1000)
+loo.a.21 = loo(fit.a.21)
 
-fit.a.22 = brm(bf(est.mass.g ~ 1 + log(Weight) + (1|no.trip.site),
+fit.a.22 = brm(bf(est.mass.g ~ 1 + (1|no.trip.site),
                   hu ~ ts.mean.2 + tss.2 + drift.mass.2 + (1|no.trip.site)), 
             data = dat.in,
             family = "hurdle_lognormal",
             chains = 3, iter = 1000)
+loo.a.22 = loo(fit.a.22)
 
-fit.a.23 = brm(bf(est.mass.g ~ 1 + log(Weight) + (1|no.trip.site),
+fit.a.23 = brm(bf(est.mass.g ~ 1 + (1|no.trip.site),
                   hu ~ density.2 + mean.temp.2 + tss.2 + (1|no.trip.site)), 
             data = dat.in,
             family = "hurdle_lognormal",
             chains = 3, iter = 1000)
+loo.a.23 = loo(fit.a.23)
 
-fit.a.24 = brm(bf(est.mass.g ~ 1 + log(Weight) + (1|no.trip.site),
+fit.a.24 = brm(bf(est.mass.g ~ 1 + (1|no.trip.site),
                   hu ~ density.2 + mean.temp.2 + drift.mass.2 + (1|no.trip.site)), 
             data = dat.in,
             family = "hurdle_lognormal",
             chains = 3, iter = 1000)
+loo.a.24 = loo(fit.a.24)
 
-fit.a.25 = brm(bf(est.mass.g ~ 1 + log(Weight) + (1|no.trip.site),
+fit.a.25 = brm(bf(est.mass.g ~ 1 + (1|no.trip.site),
                   hu ~ density.2 + tss.2 + drift.mass.2 + (1|no.trip.site)), 
             data = dat.in,
             family = "hurdle_lognormal",
             chains = 3, iter = 1000)
+loo.a.25 = loo(fit.a.25)
 
-fit.a.26 = brm(bf(est.mass.g ~ 1 + log(Weight) + (1|no.trip.site),
+fit.a.26 = brm(bf(est.mass.g ~ 1 + (1|no.trip.site),
                   hu ~ mean.temp.2 + tss.2 + drift.mass.2 + (1|no.trip.site)), 
             data = dat.in,
             family = "hurdle_lognormal",
             chains = 3, iter = 1000)
+loo.a.26 = loo(fit.a.26)
 
 
 
 # hu ~ ts.mean.2 + density.2 + mean.temp.2 + tss.2 + drift.mass.2 + (1|no.trip.site)),
 # 4 variable models 
 
-fit.a.27 = brm(bf(est.mass.g ~ 1 + log(Weight) + (1|no.trip.site),
+fit.a.27 = brm(bf(est.mass.g ~ 1 + (1|no.trip.site),
                   hu ~ density.2 + mean.temp.2 + tss.2 + drift.mass.2 + (1|no.trip.site)), 
             data = dat.in,
             family = "hurdle_lognormal",
             chains = 3, iter = 1000)
+loo.a.27 = loo(fit.a.27)
 
-fit.a.28 = brm(bf(est.mass.g ~ 1 + log(Weight) + (1|no.trip.site),
+fit.a.28 = brm(bf(est.mass.g ~ 1 + (1|no.trip.site),
                   hu ~ ts.mean.2 + mean.temp.2 + tss.2 + drift.mass.2 + (1|no.trip.site)), 
             data = dat.in,
             family = "hurdle_lognormal",
             chains = 3, iter = 1000)
+loo.a.28 = loo(fit.a.28)
 
-fit.a.29 = brm(bf(est.mass.g ~ 1 + log(Weight) + (1|no.trip.site),
+fit.a.29 = brm(bf(est.mass.g ~ 1 + (1|no.trip.site),
                   hu ~ ts.mean.2 + density.2 + tss.2 + drift.mass.2 + (1|no.trip.site)), 
             data = dat.in,
             family = "hurdle_lognormal",
             chains = 3, iter = 1000)
+loo.a.29 = loo(fit.a.29)
 
-fit.a.30 = brm(bf(est.mass.g ~ 1 + log(Weight) + (1|no.trip.site),
+fit.a.30 = brm(bf(est.mass.g ~ 1 + (1|no.trip.site),
                   hu ~ ts.mean.2 + density.2 + mean.temp.2 + drift.mass.2 + (1|no.trip.site)), 
             data = dat.in,
             family = "hurdle_lognormal",
             chains = 3, iter = 1000)
+loo.a.30 = loo(fit.a.30)
 
-fit.a.31 = brm(bf(est.mass.g ~ 1 + log(Weight) + (1|no.trip.site),
+fit.a.31 = brm(bf(est.mass.g ~ 1 + (1|no.trip.site),
                   hu ~ ts.mean.2 + density.2 + mean.temp.2 + tss.2 + (1|no.trip.site)), 
             data = dat.in,
             family = "hurdle_lognormal",
             chains = 3, iter = 1000)
+loo.a.31 = loo(fit.a.31)
 
 
 # 5 variable models
-fit.a.32 = brm(bf(est.mass.g ~ 1 + log(Weight) + (1|no.trip.site),
+fit.a.32 = brm(bf(est.mass.g ~ 1 + (1|no.trip.site),
                   hu ~ ts.mean.2 + density.2 + mean.temp.2 + tss.2 + drift.mass.2 + (1|no.trip.site)),
                data = dat.in,
                family = "hurdle_lognormal",
                chains = 3, iter = 1000)
+loo.a.32 = loo(fit.a.32)
 
 
 waic(fit.a.1, fit.a.2, fit.a.3, fit.a.4, fit.a.5, fit.a.6, fit.a.7, fit.a.8,
@@ -272,6 +304,29 @@ waic(fit.a.1, fit.a.2, fit.a.3, fit.a.4, fit.a.5, fit.a.6, fit.a.7, fit.a.8,
      fit.a.16, fit.a.17, fit.a.18, fit.a.19, fit.a.20, fit.a.21,fit.a.22,
      fit.a.23, fit.a.24, fit.a.25, fit.a.26, fit.a.27, fit.a.28, fit.a.29,
      fit.a.30, fit.a.31, fit.a.32)
+
+
+loo_compare(loo.a.1, loo.a.2, loo.a.3, loo.a.4, loo.a.5, loo.a.6, loo.a.7, loo.a.8,
+            loo.a.9, loo.a.10, loo.a.11, loo.a.12, loo.a.13, loo.a.14, loo.a.15,
+            loo.a.16, loo.a.17, loo.a.18, loo.a.19, loo.a.20, loo.a.21,loo.a.22,
+            loo.a.23, loo.a.24, loo.a.25, loo.a.26, loo.a.27, loo.a.28, loo.a.29,
+            loo.a.30, loo.a.31, loo.a.32)
+
+
+# old.loo = list(loo.a.1, loo.a.2, loo.a.3, loo.a.4, loo.a.5, loo.a.6, loo.a.7, loo.a.8,
+#             loo.a.9, loo.a.10, loo.a.11, loo.a.12, loo.a.13, loo.a.14, loo.a.15,
+#             loo.a.16, loo.a.17, loo.a.18, loo.a.19, loo.a.20, loo.a.21,loo.a.22,
+#             loo.a.23, loo.a.24, loo.a.25, loo.a.26, loo.a.27, loo.a.28, loo.a.29,
+#             loo.a.30, loo.a.31, loo.a.32)
+
+
+
+
+loo(fit.a.12, reloo = TRUE)
+
+dat.in[c(850,1147,1148),]
+
+out = lapply(fit.list, loo())
 
 #-----------------------------------------------------------------------------#
 # fit a model set... log-normal
@@ -489,42 +544,5 @@ waic(fit.b.1, fit.b.2, fit.b.3, fit.b.4, fit.b.5, fit.b.6, fit.b.7, fit.b.8,
      fit.b.30, fit.b.31, fit.b.32)
 
 
-
-
-
-
 #-----------------------------------------------------------------------------#
-# plotting / examining best model  
-
-fit.3 = brm(bf(est.mass.g ~ ts.mean.2 + density.2 + mean.temp.2 + tss.2 + drift.mass.2 + log(Weight) + (1|no.trip.site),
-               hu ~ ts.mean.2 + density.2 + mean.temp.2 + tss.2 + drift.mass.2 + (1|no.trip.site)), 
-            data = dat.in, 
-            family = "hurdle_lognormal",
-            chains = 3, iter = 1000)
-
-
-
-my.effects = data_grid(dat.in, ts.mean.2, no.trip.site) %>% add_fitted_draws(fit.3)
-
-
-
-fixef(fit.3)
-
-
-new.dat = data_grid(dat.in, ts.mean.2, no.trip.site)
-
-
-test = predict(fit.3, newdata = new.dat)
-
-windows(record = TRUE, xpos = 25)
-
-plot(marginal_effects(fit.b.32, method = "fitted"), ask = FALSE)
-# marginal_effects(fit.3, method = "fitted", re_formula = NULL) # includes the random effects
-marginal_smooths(fit.2, method = "fitted")
-
-plot(fit.3)
-
-
-
-fitted_values = fitted(fit.3)
 
